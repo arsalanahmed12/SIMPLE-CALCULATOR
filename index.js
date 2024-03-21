c5c3c6c0a1af664fdb5524412c0a1f93563d59f0
arsalanahmed12 <khanarsal884@gmail.com>
2024-03-21 16:11:07 +0500

    Simple Calculator

---
+++ index.js
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Plaese Enter The First value", type: "number", name: "FirstNumber" },
+    { message: "Please Enter The Second value", type: "number", name: "SecondNumber" },
+    {
+        message: "Which Opeartion Do You Want To Perfom",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+if (answer.operator === "Addition") {
+    console.log(answer.FirstNumber + answer.SecondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.FirstNumber - answer.SecondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.FirstNumber * answer.SecondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.FirstNumber / answer.SecondNumber);
+}
+else
+    console.log("Please Select Valid Operator");
